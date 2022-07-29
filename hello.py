db53c1ac8fdc8127b65e255a7f014d569cc6aab6
Too large to fit in the margin <qgbcs@outlook.com>
2022-07-30 02:23:16 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
 #本git库不更新 ，依赖的 代码库也不更新？
-from qgb import py,U,T,N,F
+from qgb import py,U,T,F
 
-from qgb.N.mirror_cache import app
+
+
+
+def app(env, start_response):
+    code=env['PATH_INFO'][1:]
+    code=T.url_decode(code)
+    if code.endswith('/'):code=code[:-1]
+    
+    # r=f'''{U.stime()}\n{env}\n{"="*77}\n{start_response}'''
+    start_response('200 OK', [('Content-Type','text/plain;charset=utf-8')])
+    return [U.execResult(code,globals=py.globals(),locals=py.locals())]
