963856f7108de620340910de692563cefdc1d808
Too large to fit in the margin <qgbcs@outlook.com>
2022-07-30 02:33:48 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,11 +5,13 @@ from qgb import py,U,T,F
 
 
 def app(env, start_response):
-    #U.pprint(env)
-    import requests
-    code=requests.get('https://qmm0.pythonanywhere.com/-r=code',).text
+    #import requests
+    #code=requests.get('https://qmm0.pythonanywhere.com/-r=code',).text
     
     # r=f'''{U.stime()}\n{env}\n{"="*77}\n{start_response}'''
+    
+    code=T.url_decode(env['RAW_URI'][1:] )
+    
     start_response('200 OK', [('Content-Type','text/plain;charset=utf-8')])
     rb=U.execResult(code,globals=py.globals(),locals=py.locals()).encode('utf-8')
     return [rb]
